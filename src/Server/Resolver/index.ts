7c7abff7b431af6ba8320a04996f7d24c2b4a6c0
Guillermo Morán <guillermomoran@MinideGuillermo.bbrouter>
2022-11-14 23:18:29 -0300

    Integro multer Agrego configuraciones de aws (credenciales gekko por mientras) Agrego logica subida de archivos

--- src/Server/Resolver/index.ts
+++ src/Server/Resolver/index.ts
@@ -3,10 +3,12 @@ import { Instructions } from "./Instructions";
 import { Onboarding } from "./Onboarding";
 import { Inspection } from "./Inspection";
 import { Test } from "./Test";
+import { AwsS3 } from "./AwsS3";
 
 export const Resolvers: NonEmptyArray<Function> | NonEmptyArray<string> = [
   Onboarding,
   Instructions,
   Inspection,
+  AwsS3,
   Test,
 ];
