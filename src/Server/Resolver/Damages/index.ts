ea3210f93acf5efb47a6dd69a9ec63782740e343
Franco Romero <fromero.olivares01@outlook.com>
2022-11-17 16:00:12 -0300

    [REFACTOR] Damage

--- src/Server/Resolver/Damages/index.ts
+++ src/Server/Resolver/Damages/index.ts
@@ -1,87 +1,49 @@
 import {
   Arg,
-  Args,
   Ctx,
-  Mutation,
   Query,
   Resolver,
   UseMiddleware,
 } from "type-graphql";
 
-import ORM from "../../Config/DataSource";
 import { ResponseSP, ResponseSP2D } from "../../../Services/ValidateSP";
 import { ContextLET } from "../..";
 import { InspectionAccess } from "../../Middleware/InspectionAccess";
 import { Damage } from "../../../Core/Schemas/Screen/Damage";
 import { DamageElementsSchema } from "../../../Core/Schemas/Screen/Damage/DamageElements";
-
-const db_instance = new ORM();
+import { CALL_PA_ELEMENTS_STRUCTURE_STEP_4, CALL_PA_STEP_FOUR, CALL_PA_STRUCTURE_STEP_4 } from "../../../Services/StoreProcedure";
+import { ValidateIDInspection, ValidateIDNumber } from "../../../Services/ValidateArgs";
 
 @Resolver()
 export class Damages {
-  async CALL_PA_STEP_FOUR<T>(ID_INSPECTION: Number): Promise<Array<Array<T>>> {
-    return db_instance.connection.query("EXEC PA_STEP_FOUR :ID_INSPECTION", {
-      replacements: { ID_INSPECTION },
-    }) as any;
-  }
-
-  async CALL_PA_STRUCTURE_STEP_4<T>(
-    ID_INSPECTION: Number
-  ): Promise<Array<Array<T>>> {
-    return db_instance.connection.query(
-      "EXEC PA_STRUCTURE_STEP_4 :ID_INSPECTION",
-      {
-        replacements: { ID_INSPECTION },
-      }
-    ) as any;
-  }
-
-  async CALL_PA_ELEMENTS_STRUCTURE_STEP_4<T>(
-    ID_INSPECCION: Number,
-    ID_STRUCTURE_STEP_4: Number,
-  ): Promise<Array<Array<T>>> {
-    return db_instance.connection.query(
-      "EXEC PA_ELEMENTS_STRUCTURE_STEP_4  :ID_INSPECCION, :ID_STRUCTURE_STEP_4",
-      {
-        replacements: { 
-          ID_INSPECCION, 
-          ID_STRUCTURE_STEP_4 
-        },
-      }
-    ) as any;
-  }
-
   @UseMiddleware(InspectionAccess)
-  @Query((returns) => Damage, { name: "Damages", description: "Query que obtiene estructura de datos y diferentes propiedades para armar pantalla", })
+  @Query((returns) => Damage, { 
+    name: "Damages", 
+    description: "Query que obtiene estructura de datos y diferentes propiedades para armar pantalla", 
+  })
   async Damages(@Ctx() ctx: ContextLET) {
-    if (typeof ctx.inspection?.ID_INSPECTION === "undefined") {
-      throw new Error("Token incorrecto");
-    }
+    const ID_INSPECTION = ValidateIDInspection(ctx.inspection?.ID_INSPECTION);
     return {
-      ...ResponseSP2D(await this.CALL_PA_STEP_FOUR<Damage>(ctx.inspection?.ID_INSPECTION)),
-      structure: ResponseSP(await this.CALL_PA_STRUCTURE_STEP_4(ctx.inspection?.ID_INSPECTION))
+      ...ResponseSP2D(await CALL_PA_STEP_FOUR<Damage>(ID_INSPECTION)),
+      structure: ResponseSP(await CALL_PA_STRUCTURE_STEP_4(ID_INSPECTION))
     };
   }
 
   @UseMiddleware(InspectionAccess)
-  @Query((returns) => [DamageElementsSchema], { // [DamageElementsSchema]
+  @Query((returns) => [DamageElementsSchema], {
     name: "DamageElements",
-    description: "Query que obtiene valores de opciones daños",
+    description: "Query que obtiene elementos de daño",
   })
   async DamageElements(
     @Arg("ID_STRUCTURE_STEP_4") ID_STRUCTURE_STEP_4: number,
     @Ctx() ctx: ContextLET
   ) {
-    if (
-      typeof ctx.inspection?.ID_INSPECTION === "undefined" ||
-      !ID_STRUCTURE_STEP_4
-    ) {
-      throw new Error("Token incorrecto");
-    }
+    const ID_INSPECTION = ValidateIDInspection(ctx.inspection?.ID_INSPECTION);
+    const ID_STRUCTURE = ValidateIDNumber(ID_STRUCTURE_STEP_4);
     return ResponseSP<DamageElementsSchema>(
-      await this.CALL_PA_ELEMENTS_STRUCTURE_STEP_4<DamageElementsSchema>(
-        ctx.inspection.ID_INSPECTION,
-        ID_STRUCTURE_STEP_4,
+      await CALL_PA_ELEMENTS_STRUCTURE_STEP_4<DamageElementsSchema>(
+        ID_INSPECTION,
+        ID_STRUCTURE,
       )
     );
   }
